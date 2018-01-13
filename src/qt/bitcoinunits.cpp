c7173d270e3f3edfb775cd7532a3b3105e8dc27d
Lubosz Sarnecki <lubosz@gmail.com>
2018-01-13 03:16:04 +0100

    rename: s/LTC/elsm/g

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("elsm");
+    case mBTC: return QString("melsm");
+    case uBTC: return QString::fromUtf8("μelsm");
     default: return QString("???");
     }
 }
